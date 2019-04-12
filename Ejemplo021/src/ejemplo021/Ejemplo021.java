21151d6d04f080a07fef5704693e0f71765d73d1
Carlos Vargas <carvarjr_95@hotmail.com>
2019-04-12 09:20:12 -0500

    ejemplo de presentacion de datos en pantalla

--- Ejemplo021/src/ejemplo021/Ejemplo021.java
+++ Ejemplo021/src/ejemplo021/Ejemplo021.java
@@ -12,10 +12,25 @@ package ejemplo021;
 public class Ejemplo021 {
 
     /**
+     * Asi para crear un comentario largo
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Este metodo es el principal de mi clase
+        // Comentario corto
+        String NombresPersona; // Es la variable para almacenar un nombre 
+        String ApellidosPersona; // Es la variable para almacenar un apellido
+
+        NombresPersona = "Carlos Andrés";
+        ApellidosPersona = "Vargas Ramírez";
+        
+        System.out.println("Mis nombres son: " + NombresPersona 
+                + "\n\t Mis Apellidos son: " + ApellidosPersona); 
+        // \n significa salto de linea
+        // \t significa dar tabulacion
+        //Las lineas de codigo no pueden ser mayor que la columna 80
     }
-    
+
 }
